55079f8c9d1d8fa6b001ec43a404b025f66801d3
tanianasrin1 <tasrineva169@gmail.com>
2022-04-16 16:52:13 +0600

    shipment fixed

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import {  useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 import './Login.css';
 
@@ -19,7 +19,8 @@ const Login = () => {
      
     const navigate = useNavigate();
     
-
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
       
     const handleEmailBlur = event =>{
         setEmail(event.target.value);
@@ -29,7 +30,7 @@ const Login = () => {
     }
 
     if(user){
-        navigate('/')
+        navigate(from, {replace: true});
     }
 
     const handleUserSignIn = event =>{
--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -31,7 +31,7 @@ const Orders = () => {
             </div>
             <div className="cart-container">
                 <Cart cart={cart}>
-                        <button onClick={()=>navigate('/inventory')}>Proceed Checkout </button>
+                        <button onClick={()=>navigate('/Shipment')}>Proceed shipping </button>
                 </Cart>
             </div>
         </div>
